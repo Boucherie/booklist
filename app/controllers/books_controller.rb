ebfdf717b70f56d741e36d932ce3b967ea38ae63
Emily Boucher <emi.boucher@gmail.com>
2017-06-27 21:51:04 -0400

    added text rendering and index view, csv exporting, json file

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,20 @@ class BooksController < ApplicationController
     @books = Book.all
     respond_to do |format|
       format.html
+      format.text
+
+      format.csv do
+        render plain: @books.generate_csv(@books)
+      end
+
+      format.json do
+        render json: @books.map { |book|
+        {
+          title: book.title,
+          author: book.author
+        }
+      }
+      end
     end
   end
-
 end
